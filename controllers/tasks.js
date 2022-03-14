a27907be2226e5b5c18f90dfe207accc1c7126a4
Hez <mokuahesbon254@gmail.com>
2022-03-14 13:16:24 +0300

    Changes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
-const Task = require("../models/Task");
+const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
-const { createCustomError } = require("../errors/customError");
+const { createCustomError } = require('../errors/customError')
+
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
@@ -15,31 +16,30 @@ const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`No task with id : ${taskID}`, 404));
-  }
-
-  res.status(200).json({ task });
-});
-const deleteTask = asyncWrapper(async (req, res, next) => {
-  const { id: taskID } = req.params;
-  const task = await Task.findOneAndDelete({ _id: taskID });
-  if (!task) {
-    return next(createCustomError(`No task with id : ${taskID}`, 404));
+    return next(createCustomError(`No task with id: ${taskID}` , 404))
   }
   res.status(200).json({ task });
 });
-const updateTask = asyncWrapper(async (req, res, next) => {
-  const { id: taskID } = req.params;
 
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true,
   });
-
   if (!task) {
-    return next(createCustomError(`No task with id : ${taskID}`, 404));
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
   }
 
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
+  }
   res.status(200).json({ task });
 });
 
