359f60cb00bd38a16695370c3abcd6beee457244
Hez <mokuahesbon254@gmail.com>
2022-03-14 13:01:28 +0300

    Changes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,6 +4,7 @@ const { createCustomError } = require('../errors/customError')
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
+  
   res.status(200).json({ tasks });
 });
 
