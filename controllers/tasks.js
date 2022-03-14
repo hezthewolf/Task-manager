ac6c3c21b16141e1481f22b703749c1a51af0d12
Hez <mokuahesbon254@gmail.com>
2022-03-14 13:19:06 +0300

    Changes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,6 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
-const { createCustomError } = require('../errors/customError')
+const { createCustomError } = require("../errors/customError");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -8,15 +8,20 @@ const getAllTasks = asyncWrapper(async (req, res) => {
 });
 
 const createTask = asyncWrapper(async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  const tasks = await Task.find({});
+  if (tasks === req.body) {
+    console.log("Task is present");
+  } else {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  }
 });
 
 const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`No task with id: ${taskID}` , 404))
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
